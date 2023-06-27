7a6d1ec512787b05d5a4b6a033edf04e5aaaecea
egor-davydov <a.egordavidov@gmail.com>
2023-06-27 16:46:51 +0300

    feat: add SyringeMove

---
+++ src/JellyDye/Assets/Code/Gameplay/Syringe/SyringeMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Code.Gameplay.Syringe
+{
+  public class SyringeMove : MonoBehaviour
+  {
+    [SerializeField, Range(0, 0.05f)] private float _moveSpeed = 0.01f;
+    
+    private Vector3 _previousMousePosition;
+
+    private void Update()
+    {
+      if (Input.GetMouseButton(0))
+      {
+        Vector3 moveDelta = Input.mousePosition - _previousMousePosition;
+        transform.position += new Vector3(moveDelta.x, 0, moveDelta.y) * _moveSpeed;
+      }
+      _previousMousePosition = Input.mousePosition;
+    }
+  }
+}
