e49805c2fdd49e638d339ba64de765dc7cd2d822
egor-davydov <a.egordavidov@gmail.com>
2023-07-08 15:04:20 +0300

    feat: fix the position of jelly

---
+++ src/JellyDye/Assets/Code/Gameplay/Logic/FixObiParticles.cs
@@ -0,0 +1,35 @@
+﻿using Obi;
+using UnityEngine;
+
+namespace Code.Gameplay.Logic
+{
+  public class FixObiParticles : MonoBehaviour
+  {
+    [SerializeField] private ObiActor _obiActor;
+    [SerializeField] private Transform _anchorTransform;
+    [SerializeField] private float _anchorRadius;
+
+    private ObiSolver ObiSolver => _obiActor.solver;
+
+    private void Start()
+    {
+      foreach (int solverIndex in _obiActor.solverIndices)
+      {
+        float particleDistanceToAnchor = Vector3.Distance(_obiActor.GetParticlePosition(solverIndex), _anchorTransform.position);
+        if (particleDistanceToAnchor > _anchorRadius)
+          continue;
+        
+        ObiSolver.velocities[solverIndex] = Vector3.zero;
+        ObiSolver.invMasses[solverIndex] = 0;
+      }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+      Gizmos.color = Color.red;
+      Gizmos.DrawWireSphere(_anchorTransform.position, _anchorRadius);
+    }
+#endif
+  }
+}
