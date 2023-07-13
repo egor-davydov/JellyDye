e97de07cf68aafe5999c862ec5504cd93e98c6fd
egor-davydov <a.egordavidov@gmail.com>
2023-07-13 21:01:17 +0300

    feat: add level reload button

---
+++ src/JellyDye/Assets/Code/Gameplay/Hud/ReloadLevel.cs
@@ -0,0 +1,24 @@
+﻿using Code.Infrastructure.States;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Code.Gameplay.Hud
+{
+  public class ReloadLevel : MonoBehaviour
+  {
+    [SerializeField] private Button _reloadLevelButton;
+    private GameStateMachine _gameStateMachine;
+
+    [Inject]
+    public void Construct(GameStateMachine gameStateMachine) => 
+      _gameStateMachine = gameStateMachine;
+
+    private void Awake() => 
+      _reloadLevelButton.onClick.AddListener(ReloadLevelClick);
+
+    private void ReloadLevelClick() => 
+      _gameStateMachine.Enter<LoadLevelState, string>(SceneManager.GetActiveScene().name);
+  }
+}
