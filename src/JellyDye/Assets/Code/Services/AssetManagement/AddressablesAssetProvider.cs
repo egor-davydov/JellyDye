c2918dcc710851284d7f5d20215b0b20e5c33aa4
egor-davydov <a.egordavidov@gmail.com>
2024-01-29 17:26:16 +0300

    install addressables

---
+++ src/JellyDye/Assets/Code/Services/AssetManagement/AddressablesAssetProvider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.Services.AssetManagement
+{
+  public class AddressablesAssetProvider : IAssetProvider
+  {
+    public T Load<T>(string assetPath) where T : Object
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
