34618abcb39c0e08a9e997878dcfbc99af8affda
Egor Davydov <a.egordavidov@gmail.com>
2024-11-29 15:51:42 +0300

    refactor: sound button

--- src/JellyDye/Assets/Code/Gameplay/UI/Hud/Sound/SoundButton.cs
+++ src/JellyDye/Assets/Code/Gameplay/UI/Hud/Sound/SoundButton.cs
@@ -8,27 +8,43 @@ namespace Code.Gameplay.UI.Hud.Sound
   public class SoundButton : MonoBehaviour
   {
     [SerializeField] private UIAudio _uiAudio;
-    [SerializeField] private bool _turnSoundOn;
     [SerializeField] private Button _soundButton;
-    
+    [SerializeField] private Sprite _soundOnIcon;
+    [SerializeField] private Sprite _soundOffIcon;
+    [SerializeField] private Image _soundIconImage;
+
     private AudioService _audioService;
 
     [Inject]
-    public void Construct(AudioService audioService) => 
+    public void Construct(AudioService audioService) =>
       _audioService = audioService;
 
-    private void Awake() => 
+    private void Awake()
+    {
+      _audioService.OnAudioStateChanged += OnAudioStateChanged;
       _soundButton.onClick.AddListener(OnSoundClick);
+      OnAudioStateChanged(!_audioService.IsAudioMuted);
+    }
+
+    private void OnDestroy() =>
+      _audioService.OnAudioStateChanged -= OnAudioStateChanged;
 
     private void OnSoundClick()
     {
+      bool isTurningAudioOn = _audioService.IsAudioMuted;
       _uiAudio.PlayClick();
-
-      if (_turnSoundOn)
+      OnAudioStateChanged(isTurningAudioOn);
+      if (isTurningAudioOn)
         _audioService.UnMuteGame();
       else
         _audioService.MuteGame();
     }
 
+    private void OnAudioStateChanged(bool isTurningAudioOn)
+    {
+      _soundIconImage.sprite = isTurningAudioOn
+        ? _soundOnIcon
+        : _soundOffIcon;
+    }
   }
 }
